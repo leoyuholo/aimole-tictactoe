b0838d47b6ca0b716ace1356e8032e5509079c9b
leoyuholo <leoyuholo@gmail.com>
2016-02-24 16:45:31 +0800

    remove alert

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -50,7 +50,6 @@ class App extends React.Component {
     }
 
     componentDidMount() {
-        alert('submit button clicked.');
         this.setState({
             results: gameBoard,
             submitted: true,
